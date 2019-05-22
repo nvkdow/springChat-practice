7b1b0e8b86c86a94d00b57180e7e72c7abd7a00d
Ryu JiHoon <dbwglns123@gmail.com>
2019-05-22 12:41:38 +0900

    init spring mvc project

---
+++ src/main/java/com/chatter/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.chatter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("")
+@Controller
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showHome() {
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Rang
+  Date: 2019-05-22
+  Time: 오후 12:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>HOME</title>
+</head>
+<body>
+test
+</body>
+</html>
